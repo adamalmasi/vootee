91f05c1e492f7aea935376b09bb1be21594a5cb4
Gergely Vargha <gergelyvargha@192.168.68.59>
2026-05-16 16:28:52 +0200

    fix: restore jsdom test environment with nanoid ESM transform

--- jest.config.ts
+++ jest.config.ts
@@ -2,19 +2,14 @@ import type { Config } from 'jest'
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig: { jsx: 'react' } }],
     '^.+\\.js$': ['ts-jest', { useESM: true }],
   },
-  testPathIgnorePatterns: ['/node_modules/'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(nanoid)/)',
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  transformIgnorePatterns: ['node_modules/(?!(nanoid)/)'],
 }
 
 export default config
